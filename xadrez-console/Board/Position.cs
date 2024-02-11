08a60e6bb800d7c0b927b17057a3c2e0d25cde80
Victor R. Silva <victorr.silva005@gmail.com>
2024-02-11 07:52:53 -0300

    Created class Position

---
+++ xadrez-console/Board/Position.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.Board
+{
+    internal class Position
+    {
+        public int Linha {  get; set; }
+        public int Coluna { get; set; }
+
+        public Position() { }
+
+        public Position(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return Linha + ", " + Coluna;
+        }
+    }
+}
---
+++ xadrez-console/Program.cs
@@ -0,0 +1,11 @@
+﻿using xadrez_console.Board;
+namespace xadrez_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
